f99ae4ff9ead815a72d04ffb9fa07b4e2e04b4c2
Arslan Gul <arslangul93@hotmail.com>
2016-06-22 12:51:33 +0500

    new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,12 +31,43 @@ module.exports = function(grunt) {
           }
 
 
+      },
+      protractor_webdriver: {
+          target: {
+              options: {
+                  path: 'node_modules/protractor/bin/',
+                  command: 'webdriver-manager start'
+              }
+          }
+      },
+      protractor: {
+
+          target: {
+              options: {
+                  configFile: "Tests/conf.js", // Target-specific config file
+                  args: {} // Target-specific arguments
+              }
+          }
+      },
+      "qunit-cov": {
+          test: {
+              minimum: 0.9,
+              srcDir: 'src',
+              outDir: 'testResults',
+              testFiles: ['Tests/conf.js']
+          }
       }
   });
+
+    grunt.registerTask('p:test', [
+        'protractor_webdriver',
+        'protractor'
+    ]);
   grunt.loadNpmTasks('grunt-jenkins');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-testem');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-qunit-cov');
 
     grunt.registerTask('default', ['jshint', 'testem', 'clean']);
     grunt.registerTask('jenkins', ['jshint', 'testem', 'clean' ]);
